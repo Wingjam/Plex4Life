facd772c84e0f261c471c695e7795dcdc56bec05
Jonathan Martineau <jo_martineau_7@hotmail.com>
2017-08-28 21:24:14 -0400

    Added PayPal donation button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Donations</h2>
+        <h2>
+          Donations
+          <a href="https://www.paypal.me/plex4life/20">
+            <img id="PayPalButton" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="Donate with Paypal" />
+          </a>
+        </h2>
         <SimplePieChart/>
         <h2>Kudos</h2>
         <li>Kudos to everyone who donate!</li>

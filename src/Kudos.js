eaddb3213812d26f64bb9804feabb029f7ddffc3
Jonathan Martineau <jo_martineau_7@hotmail.com>
2017-10-02 21:22:13 -0400

    Added Francis to Kudos

--- src/Kudos.js
+++ src/Kudos.js
@@ -7,6 +7,7 @@ class Kudos extends Component {
         <h2>Kudos</h2>
         <ul>
           <li>Kudos to <b>everyone</b> who donate!</li>
+          <li>Kudos to <b>Francis Gagnon</b> for helping hosting and playing badly at NHL.</li>
           <li>Kudos to <b>Jonathan Raymond</b> for hosting and helping in hardware.</li>
           <li>Kudos to <b>Raphaël Cayouette</b> for helping with Plex admin and content.</li>
         </ul>
